0d121c483da85908f328f74f0070f4ed191f2d3d
emuhlestein <emuhlestein@yahoo.com>
2020-02-19 19:44:35 -0600

    Save changes

--- posts/models.py
+++ posts/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     text = models.TextField()
+
+    # return a string representation of the object
+    def __str__(self):
+        return self.text
